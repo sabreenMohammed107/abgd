dc21bd6b754c34ff811c380413144629b897499a
sabreenMohammed107 <sabreenm312@gmail.com>
2022-05-27 21:40:04 +0200

    test

--- resources/views/web/layout/head.blade.php
+++ resources/views/web/layout/head.blade.php
@@ -2,6 +2,26 @@
 <html lang="en">
 <head>
 	<title>Abgad</title>
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org/",
+          "@type": "Recipe",
+          "name": "ABGAD",
+          "author": {
+            "@type": "Person",
+            "name": "Senior steps"
+          },
+          "datePublished": "2022-05-27",
+          "description": "ABGAD very important to give aloan to parents to schools ",
+          "prepTime": "PT20M"
+        }
+        </script>
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="description" content="ABGAD very important to give aloan to parents to schools. ABGAD very important to give aloan to parents to schools">
+        <meta name="keywords" content="ABGAD,aloan,قروض ,schools,parents,ابجد ,مدارس">
+        <meta name="author" content="sabreen mohammed & senior steps">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Favicon -->
